8bd9361691564c908ec226b8dddeb48b071ef11d
Tristan de Cacqueray <tristan@midirus.com>
2025-10-05 01:00:48 +0200

    Fix head/tail warnings and add comments

--- tidal-mondo/src/Mondo/Eval.hs
+++ tidal-mondo/src/Mondo/Eval.hs
@@ -6,6 +6,7 @@
 
 module Mondo.Eval where
 
+import Data.List.NonEmpty qualified as NE
 import GHC.Float (float2Double)
 
 import Sound.Tidal.Core ((#), (|+|))
@@ -34,37 +35,39 @@ data TidalPat a
     | After (T.Pattern a -> T.ControlPattern)
 
 eval_list :: [MondoExpr] -> Either ParseError T.ControlPattern
-eval_list xs = case xs of
+eval_list es = case es of
     -- s is expected to be at the begining of a pattern
-    (Com "s" : rest) -> T.fastCat <$> traverse (resolve_seq "s" T.sound getString) rest
+    (Com "s" : rest) -> T.fastCat <$> traverse (eval_last "s" T.sound getString) rest
     (Com "n" : rest)
         | -- note after sound, e.g. 's sine # n c2'
-          (params, MList sound@(Com "s" : _)) <- (init rest, last xs) -> do
+          Just (params, MList sound@(Com "s" : _)) <- unsnoc rest -> do
             soundControl <- eval_list sound
-            noteControl <- T.fastCat <$> traverse (resolve_seq "n" T.n getNote) params
+            noteControl <- T.fastCat <$> traverse (eval_last "n" T.n getNote) params
             pure $ soundControl |+| noteControl
         | -- standalone n, without sound, e.g. for midi. This is also expected to be at the begining of a pattern
           otherwise ->
-            T.fastCat <$> traverse (resolve_seq "n" T.n getNote) rest
-    (Com "lpf" : xs) -> eval_control getDouble (After T.cutoff) xs
-    (Com "fast" : xs) -> eval_control getTime (Before T.fast) xs
-    (Com "slow" : xs) -> eval_control getTime (Before T.slow) xs
-    (MCommand "stack" : xs) -> T.stack <$> traverse eval xs
-    x : _ -> mkError ("unexpected command: " <> show xs) (exprPos x)
+            T.fastCat <$> traverse (eval_last "n" T.n getNote) rest
+    (Com "lpf" : rest) -> eval_control getDouble (After T.cutoff) rest
+    (Com "fast" : rest) -> eval_control getTime (Before T.fast) rest
+    (Com "slow" : rest) -> eval_control getTime (Before T.slow) rest
+    (MCommand "stack" : rest) -> T.stack <$> traverse eval rest
+    x : _ -> mkError ("unexpected command: " <> show es) (exprPos x)
     [] -> mkError "expected command!" (P.newPos "input" 1 1)
   where
-    eval_control get app xs = case (init xs, last xs) of
-        (params, MList rest) -> do
-            paramPat <- T.fastCat <$> traverse (eval_pat get) params
+    eval_control get app xs
+        | Just (params, MList rest) <- unsnoc xs = do
+            paramPat <- eval_pats get params
             controlPat <- eval_list rest
             pure $ case app of
-                After app -> controlPat # app paramPat
-                Before app -> app paramPat controlPat
-        _ -> mkError ("invalid control pattern: " <> show xs) (exprPos $ head xs)
+                After aapp -> controlPat # aapp paramPat
+                Before bapp -> bapp paramPat controlPat
+        | otherwise = mkError ("invalid control pattern: " <> show xs) (exprPos $ MList xs)
 
+-- | Evaluate a list of pattern like 'bd <sd hh>'
 eval_pats :: (T.Parseable a, T.Enumerable a) => (MondoExpr -> Maybe (T.Pattern a)) -> [MondoExpr] -> Either ParseError (T.Pattern a)
 eval_pats get xs = T.fastCat <$> traverse (eval_pat get) xs
 
+-- | Evaluate a pattern like '<a b>'
 eval_pat :: (T.Parseable a, T.Enumerable a) => (MondoExpr -> Maybe (T.Pattern a)) -> MondoExpr -> Either ParseError (T.Pattern a)
 eval_pat get expr = case expr of
     (MString p) -> case T.parseBP p.value of
@@ -85,6 +88,26 @@ eval_pat get expr = case expr of
         paramPat <- eval_pat getp param
         valPat <- eval_pat get val
         pure $ app paramPat valPat
+
+-- | Evaluate the last pipe (e.g. the first pattern), like 'n' or 's', which supports 'Chaining Functions Locally'.
+eval_last :: (T.Parseable a, T.Enumerable a) => String -> (T.Pattern a -> T.ControlPattern) -> (MondoExpr -> Maybe (T.Pattern a)) -> MondoExpr -> Either ParseError T.ControlPattern
+eval_last com app get expr = case expr of
+    MList [MCommand ":", note, sound] -> do
+        soundPat <- eval_pat get sound
+        notePat <- eval_pat getFloat note
+        pure $ app soundPat |+| T.pF "n" notePat
+    MList (Com "scale" : rest)
+        | com == "n"
+        , Just (params, MList note) <- unsnoc rest -> do
+            notePat <- eval_pats getInt note
+            scalePat <- eval_pats getString params
+            pure $ T.scale scalePat notePat
+    -- `s bd (hh # lpf 42)` is desugared to `(s bd (lpf 50 sd))`, and here,
+    -- when we process `(lpf 50 sd)`, the following case rewrite it as: (lpf 50 (s sd))
+    MList xs@(MPlain _ : _) ->
+        let (i, l) = (init xs, last xs)
+         in eval_list $ i <> [MList [MPlain (Positioned com 0 0), l]]
+    _ -> app <$> eval_pat get expr
 
 getDouble :: MondoExpr -> Maybe (T.Pattern Double)
 getDouble expr = case expr of
@@ -119,25 +142,6 @@ getNote expr = case expr of
     MValue v -> Just . patWithPos $ T.Note . float2Double <$> v
     _ -> Nothing
 
-resolve_seq :: (T.Parseable a, T.Enumerable a) => String -> (T.Pattern a -> T.ControlPattern) -> (MondoExpr -> Maybe (T.Pattern a)) -> MondoExpr -> Either ParseError T.ControlPattern
-resolve_seq com app get expr = case expr of
-    MList [MCommand ":", note, sound] -> do
-        soundPat <- eval_pat get sound
-        notePat <- eval_pat getFloat note
-        pure $ app soundPat |+| T.pF "n" notePat
-    MList (Com "scale" : rest)
-        | com == "n"
-        , (params, MList note) <- (init rest, last rest) -> do
-            notePat <- eval_pats getInt note
-            scalePat <- eval_pats getString params
-            pure $ T.scale scalePat notePat
-    -- `s bd (hh # lpf 42)` is desugared to `(s bd (lpf 50 sd))`, and here,
-    -- when we process `(lpf 50 sd)`, the following case rewrite it as: (lpf 50 (s sd))
-    MList xs@(MPlain _ : _) ->
-        let (i, l) = (init xs, last xs)
-         in eval_list $ i <> [MList [MPlain (Positioned com 0 0), l]]
-    _ -> app <$> eval_pat get expr
-
 patWithPos :: Positioned a -> T.Pattern a
 patWithPos v = (T.withContext (addPos v) $ pure v.value)
 
@@ -152,6 +156,12 @@ exprPos expr = case expr of
     MPlain v -> posPos v
     MValue v -> posPos v
     MString v -> posPos v
+    MList (v : _) -> exprPos v
     _ -> (P.newPos "input" 1 1)
   where
     posPos v = P.newPos "input" v.row v.col
+
+unsnoc :: [a] -> Maybe ([a], a)
+unsnoc xs = do
+    ne <- NE.nonEmpty xs
+    pure (NE.init ne, NE.last ne)
